c37010d5bc6a5395666681cb15992647f34d1317
Cocisran <francisco.delmoral@i-vsp.com>
2025-06-30 12:19:14 -0600

    .

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("C:\\Users\\fcoem\\Documents\\work\\teper\\demo documentacion\\teper_docs_demo\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("C:\\Users\\fcoem\\Documents\\work\\teper\\demo documentacion\\teper_docs_demo\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("C:\\Users\\fcoem\\Documents\\work\\teper\\demo documentacion\\teper_docs_demo\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("C:\\Users\\fcoem\\Documents\\work\\teper\\demo documentacion\\teper_docs_demo\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+];
