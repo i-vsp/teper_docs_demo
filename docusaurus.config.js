e3e621f954a0b01e78ab32397640575b111d5f0a
Cocisran <francisco.delmoral@i-vsp.com>
2025-06-30 12:44:03 -0600

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,7 +29,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'i-vsp', // Usually your GitHub org/user name.
   projectName: 'teper_docs_demo', // Usually your repo name.
-  trailingSlash: false,
+  trailingSlash: true,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
