a5e52fb92fd628d1715e6e3176163b7e151a6592
Cocisran <francisco.delmoral@i-vsp.com>
2025-06-30 12:49:03 -0600

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,7 +32,7 @@ const config = {
   trailingSlash: true,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
+  deploymentBranch: 'gh-pages',
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
